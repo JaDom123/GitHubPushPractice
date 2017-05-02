4ae07151da0842fa5fbc6cb54c6532447d7fc371
Jason Swift <jdswift94@gmail.com>
2017-05-02 16:52:36 +0100

    commit for in loop and print yo

--- GitHubPushPractice/ViewController.swift
+++ GitHubPushPractice/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("YoYo")
+        for i in 0...100 where i % 2 == 0 {
+            print(i)
+        }
     }
 
     override func didReceiveMemoryWarning() {
